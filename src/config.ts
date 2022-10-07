31deec5cddd997a76be78df210d9edfd1edc920e
YanivK <yanivkoubbi@gmail.com>
2022-10-07 14:55:20 +0300

    Change configuration to benstalk aws

--- src/config.ts
+++ src/config.ts
@@ -4,11 +4,11 @@ dotenv.config();
 const config =
     {
         db: {
-            host: process.env.DB_HOST,
-            user: process.env.DB_USER,
-            password: process.env.DB_PASSWORD,
-            database: process.env.DB_DATABASE,
-            socketPath: process.env.DB_HOST
+          host: process.env.RDS_HOSTNAME || process.env.DB_HOST,
+          user: process.env.RDS_USERNAME || process.env.DB_USER,
+          password: process.env.RDS_PASSWORD || process.env.DB_PASSWORD,
+          database: process.env.RDS_DB_NAME || process.env.DB_DATABASE,
+          socketPath: process.env.RDS_HOSTNAME || process.env.DB_HOST
         },
         listPerPage: 10
     }
